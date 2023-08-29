a308fa75a44fb9d0d18e4dbb1a7828dcf3b9f7aa
mbenguigui <mathis.benguigui@wikimedia.fr>
2023-08-29 22:40:13 +0200

    add date of handle

--- protect_requests.py
+++ protect_requests.py
@@ -39,7 +39,7 @@ def close_dpp(section_content, article, admin, protect):
     section_content = section_content.replace(end_section, message) + end_section
 
     match = u'{{DPP début|statut=|date=<!--~~~~~-->}}'
-    replace = u'{{DPP début|statut=oui|date=<!--~~~~~-->}}'
+    replace = u'{{DPP début|statut=oui|date=~~~~~}}'
     if section_content.find(match) == -1:
         match = u'{{DPP début|statut=attente'
         replace = u'{{DPP début|statut=oui'
