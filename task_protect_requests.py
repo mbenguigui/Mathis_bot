d8335e3c8e40aeed789ab06a909bdd3ca6ef6af7
mbenguigui <mathis.benguigui@wikimedia.fr>
2024-01-03 21:37:51 +0100

    Looks like cascade parameter is not present if false anymore. Should repare protect requests.

--- task_protect_requests.py
+++ task_protect_requests.py
@@ -84,7 +84,10 @@ def check_protect(current_time, article, site, section_content):
         start_date = datetime.strptime(log['timestamp'], '%Y-%m-%dT%H:%M:%SZ')
 
         if start_date >= current_time - timedelta(minutes=10):
-            return close_dpp(section_content, article, log['revid'], log['user'], log['params']['cascade'], start_date, protect)
+            is_cascade = False
+            if 'cascade' in log['params']:
+                is_cascade = log['params']['cascade']
+            return close_dpp(section_content, article, log['revid'], log['user'], is_cascade, start_date, protect)
 
     return False, section_content
 
