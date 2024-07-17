96705a8ea22b4e44168e68d7095a9332e2ec79a7
mbenguigui <mathis.benguigui@wikimedia.fr>
2024-07-17 20:17:29 +0200

    Create request_replace_flag_in_infobox_election.py

---
+++ request_replace_flag_in_infobox_election.py
@@ -0,0 +1,32 @@
+import pywikibot, re
+from mathis_bot_tools import can_run
+from pywikibot import textlib
+
+
+def replace_flags(site):
+    file = open("liste.txt", "r", encoding='utf-8')
+
+    for page_name in file:
+        if not can_run(site):
+            break
+
+        page = pywikibot.Page(site, page_name)
+        text = page.text
+
+        templates = textlib.extract_templates_and_params(text, strip=True)
+        for template in templates:
+            if template[0] == u'Infobox Élection' and u'pays' in template[1] and template[1][u'pays'] != u'France':
+                text = re.sub(r'(pays\s+=\s+)([A-Za-z-\-]+)', r'\1France', text, count=1, flags=re.IGNORECASE)
+                page.text = text
+                page.save(u'[[WP:RBOT|Requête bot]] : [[Spécial:Lien permanent/216881998#Drapeau pour les élections de portée nationale en France|Remplacement de drapeaux pour les élections de portée nationale en France]]')
+                break
+
+
+def main():
+    site = pywikibot.Site('fr', 'wikipedia')
+
+    replace_flags(site)
+
+
+if __name__ == '__main__':
+    main()
