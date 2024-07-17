8182bdff43c3c31bbef031cb7970912e06b8ff26
mbenguigui <mathis.benguigui@wikimedia.fr>
2024-07-17 20:41:17 +0200

    Added apostrophe

--- request_replace_flag_in_infobox_election.py
+++ request_replace_flag_in_infobox_election.py
@@ -17,7 +17,7 @@ def replace_flags(site):
         templates = textlib.extract_templates_and_params(text, strip=True)
         for template in templates:
             if template[0] == u'Infobox Élection' and u'pays' in template[1] and template[1][u'pays'] != u'France':
-                text = re.sub(r'(pays\s+=\s+)([A-zÀ-ú-\-]+)', r'\1France', text, count=1, flags=re.IGNORECASE)
+                text = re.sub(r'(pays\s+=\s+)([A-zÀ-ú-\'-’-\-]+)', r'\1France', text, count=1, flags=re.IGNORECASE)
 
                 edit_summary = u'[[WP:RBOT|Requête bot]] : [[Spécial:Lien permanent/216881998#Drapeau pour les'
                 edit_summary += u' élections portée nationale en France|Remplacement de drapeaux pour les élections de'
